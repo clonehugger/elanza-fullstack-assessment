e334c889a72bb2673e8e588d22f52d2ba0d55279
Andrea Puente Sánchez <andrea.puente@udem.edu>
2021-05-28 13:29:00 -0500

    Only show open requests

--- src/OpenRequests.js
+++ src/OpenRequests.js
@@ -9,28 +9,30 @@ export const OpenRequests = () => {
             <section class="section" className="request-container">
                 {/* FOR EACH REQUEST */}
                     {requestData.map((data, key) => {
-                        return (
-                            <div className="box" key={key}>
-                                <div className="columns">
-                                    <div className="column">
-                                        <div className="has-text-grey-lighter is-uppercase">
-                                            Start time
+                        if (data.status == "open") { // only show open requests
+                            return (
+                                <div className="box" key={key}>
+                                    <div className="columns">
+                                        <div className="column">
+                                            <div className="has-text-grey-lighter is-uppercase">
+                                                Start time
+                                            </div>
+                                            {data.startTime} hrs
                                         </div>
-                                        {data.startTime} hrs
-                                    </div>
-                                    <div className="column">
-                                        <div className="has-text-grey-lighter is-uppercase">
-                                            Client
+                                        <div className="column">
+                                            <div className="has-text-grey-lighter is-uppercase">
+                                                Client
+                                            </div>
+                                            {data.name}
                                         </div>
-                                        {data.name}
-                                    </div>
-                                    <div className="column">
-                                        <button className="button is-link has-background-elanza">View details
-                                            of {key}</button>
+                                        <div className="column">
+                                            <button className="button is-link has-background-elanza">View details
+                                                of {key}</button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        );
+                            );
+                        }
                     })}
                 {/* END FOR EACH */}
 
--- src/data.js
+++ src/data.js
@@ -1,4 +1,6 @@
 export const requestData = [
-    { "name": "Jane Doe", "startTime": "14:00:00", "endTime": "16:00:00", "careKind": "household", "extra": "Elderly woman"  },
-    { "name": "John Doe", "startTime": "18:00:00", "endTime": "20:00:00", "careKind": "medical", "extra": "Elderly man with Parkinsons"  }
+    { "name": "Jane Doe", "startTime": "14:00:00", "endTime": "16:00:00", "careKind": "household", "extra": "Elderly woman", "status": "open"  },
+    { "name": "John Doe", "startTime": "18:00:00", "endTime": "20:00:00", "careKind": "medical", "extra": "Elderly man with Parkinsons", "status": "open"  },
+    { "name": "Jane Doe", "startTime": "14:00:00", "endTime": "16:00:00", "careKind": "household", "extra": "Elderly woman", "status": "closed"  }
+
 ];
